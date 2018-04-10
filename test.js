d2c97c09f94ea105c28c3eb7985f450c222991bd
Kev <lineus@gmail.com>
2018-04-10 19:19:31 -0400

    adding rungist and test files

---
+++ test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+
+const test2 = require('./test2.js');
+const test3 = require('./test3.js');
+
+test2();
+test3();
---
+++ test2.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+
+module.exports = function () {
+  process.stdout.write('Hello ');
+};
