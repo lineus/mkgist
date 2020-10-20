a9dd8d4473f66d57b3f8b3a89874e1bf0545ab0f
Kevin Huff <lineus@gmail.com>
2020-10-20 11:24:11 -0400

    updating node-fetch

--- test.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-const test2 = require('./test2.js');
-const test3 = require('./test3.js');
-
-test2();
-test3();
--- test2.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-module.exports = function () {
-  process.stdout.write('Hello ');
-};
